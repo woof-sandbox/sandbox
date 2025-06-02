17017175235d3d18eb4019e9a3d3e196f3cee0a1
pavlo-h <pavlo@woof.software>
2025-06-02 14:13:16 +0300

    tests: remove only

--- test/1_system-initialization-test.ts
+++ test/1_system-initialization-test.ts
@@ -20,7 +20,7 @@ import {
   CometConfigStruct,
 } from "../build/types/ConfigController";
 
-describe.only("System Initialization", function() {
+describe("System Initialization", function() {
     // Factories
   let _ConfigControllerFactory: ConfigControllerFactory__factory;
   let _ConfigController: ConfigControllerInitializeTest__factory;
