18f908a3c3d89a2d129f64bd8140e128838ba800
VladyslavBudiukWOOF <vlad.b@woof.software>
2025-07-02 12:26:38 +0200

    fix ts compile test/8

--- test/8_comet-ext-test.ts
+++ test/8_comet-ext-test.ts
@@ -1,22 +1,25 @@
 import { ethers, expect, makeProtocol, exp, setTotalsBasic } from "./helper/helpers";
-import type { CometHarnessInterface } from "../build/types";
+import type { CometHarness } from "../build/types";
 import type { FaucetToken, NonStandardFaucetFeeToken } from "../build/types";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+// Define the extended interface for the comet contract with additional methods
+interface ExtendedCometHarness extends CometHarness {
+  factorScale(): Promise<bigint>;
+  priceScale(): Promise<bigint>;
+  getConfiguration(): Promise<any>;
+}
 
 describe("8. CometExtension", () => {
-  let comet: CometHarnessInterface & {
-    factorScale(): Promise<bigint>;
-    priceScale(): Promise<bigint>;
-    collateralBalanceOf(addr: string, asset: string): Promise<bigint>;
-  };
-  let user: Awaited<ReturnType<typeof ethers.getSigners>>[number];
+  let comet: ExtendedCometHarness
+  let user: SignerWithAddress;
   let tokens: Record<string, FaucetToken | NonStandardFaucetFeeToken>;
 
   beforeEach(async () => {
-    ({
-      comet,
-      users: [user],
-      tokens,
-    } = await makeProtocol());
+    const protocol = await makeProtocol();
+    const baseComet = protocol.comet;
+    user = protocol.users[0];
+    tokens = protocol.tokens;
 
     const [signer] = await ethers.getSigners();
     const extAbi = [
@@ -48,7 +51,7 @@ describe("8. CometExtension", () => {
         "tuple(address collateralToken, address priceFeed, uint256 borrowCollateralFactor, uint256 liquidateCollateralFactor, uint256 liquidationFactor, uint256 supplyCap, uint256 scale)[] assetConfigs" +
         "))",
     ];
-    comet = new ethers.Contract(comet.address, [...comet.interface.fragments, ...extAbi], signer) as typeof comet;
+    comet = new ethers.Contract(baseComet.address, [...baseComet.interface.fragments, ...extAbi], signer) as ExtendedCometHarness;
 
     await setTotalsBasic(comet, { baseSupplyIndex: 2e15, baseBorrowIndex: 3e15 });
   });
