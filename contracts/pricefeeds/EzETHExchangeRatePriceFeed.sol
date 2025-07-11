cd8f83691d73d19a15f29614b227e178da4f261d
VladyslavPerederWoof <vlad.p@woof.software>
2025-07-11 17:19:55 +0300

    feat: added access control and fallback provider

--- contracts/pricefeeds/EzETHExchangeRatePriceFeed.sol
+++ contracts/pricefeeds/EzETHExchangeRatePriceFeed.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.28;
 
 import "../interfaces/IBalancerRateProvider.sol";
 import "../interfaces/IPriceFeed.sol";
+import { AccessControl } from "contracts/pricefeeds/AccessControl.sol";
 
 /**
  * @title ezETH Scaling price feed
@@ -10,7 +11,7 @@ import "../interfaces/IPriceFeed.sol";
  * price feed and returns the result
  * @author Compound
  */
-contract EzETHExchangeRatePriceFeed is IPriceFeed {
+contract EzETHExchangeRatePriceFeed is IPriceFeed, AccessControl {
     /** Custom errors **/
     error InvalidInt256();
     error BadDecimals();
@@ -24,9 +25,6 @@ contract EzETHExchangeRatePriceFeed is IPriceFeed {
     /// @notice Number of decimals for returned prices
     uint8 public immutable override decimals;
 
-    /// @notice ezETH price feed where prices are fetched from
-    address public immutable underlyingPriceFeed;
-
     /// @notice Whether or not the price should be upscaled
     bool internal immutable shouldUpscale;
 
@@ -36,6 +34,12 @@ contract EzETHExchangeRatePriceFeed is IPriceFeed {
     /// @notice The underlying token
     address public immutable override underlyingToken;
 
+    /// @notice ezETH price feed where prices are fetched from
+    address public underlyingPriceFeed;
+
+    /// @notice Fallback price feed address
+    address public fallbackPriceFeed;
+
     /**
      * @notice Construct a new ezETH scaling price feed
      * @param ezETHRateProvider The address of the underlying price feed to fetch prices from
@@ -43,8 +47,19 @@ contract EzETHExchangeRatePriceFeed is IPriceFeed {
      * @param description_ The description of the price feed
      * @param underlyingToken_ The address of the underlying token
      **/
-    constructor(address ezETHRateProvider, uint8 decimals_, string memory description_, address underlyingToken_) {
+    constructor(
+        address ezETHRateProvider,
+        address fallbackEzETHRateProvider,
+        uint8 decimals_,
+        string memory description_,
+        address underlyingToken_,
+        address owner_,
+        address dao_
+    ) AccessControl(owner_, dao_) {
+        if (ezETHRateProvider == address(0)) revert ZeroAddress();
         underlyingPriceFeed = ezETHRateProvider;
+        fallbackPriceFeed = fallbackEzETHRateProvider;
+
         if (decimals_ > 18) revert BadDecimals();
         decimals = decimals_;
         description = description_;
@@ -58,6 +73,21 @@ contract EzETHExchangeRatePriceFeed is IPriceFeed {
                 : signed256(10 ** (ezETHRateProviderDecimals - decimals_))
         );
         underlyingToken = underlyingToken_;
+    }
+
+    /**
+     * @notice Sets the price feeds for the contract
+     * @param _underlyingPriceFeed The address of the underlying price feed
+     * @param _fallbackPriceFeed The address of the fallback price feed
+     * @dev Only callable by the owner or DAO
+     * @dev Reverts if the underlying price feed address is zero
+     * @dev Fallback price feed could be zero address, which means that the fallback is not used
+     */
+    function setPriceFeeds(address _underlyingPriceFeed, address _fallbackPriceFeed) external onlyAuthorized {
+        if (_underlyingPriceFeed == address(0)) revert ZeroAddress();
+
+        underlyingPriceFeed = _underlyingPriceFeed;
+        fallbackPriceFeed = _fallbackPriceFeed;
     }
 
     /**
@@ -75,6 +105,7 @@ contract EzETHExchangeRatePriceFeed is IPriceFeed {
         returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
     {
         uint256 rate = IBalancerRateProvider(underlyingPriceFeed).getRate();
+        if (rate == 0 || rate > 1e36) rate = IBalancerRateProvider(fallbackPriceFeed).getRate();
         // protocol uses only the answer value. Other data fields are not provided by the underlying pricefeed and are not used
         // in Comet protocol
         // https://etherscan.io/address/0x387dBc0fB00b26fb085aa658527D5BE98302c84C#readProxyContract
