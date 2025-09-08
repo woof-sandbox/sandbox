e9ecb37fb7e63579e748bb8253bc4194987dabfb
VladyslavPerederWoof <vlad.p@woof.software>
2025-09-08 16:57:41 +0300

    chore: markets redeploy

--- script/separate/DeploySandboxController.s.sol
+++ script/separate/DeploySandboxController.s.sol
@@ -17,7 +17,7 @@ contract DeploySandboxController is Script {
     }
 
     function deploySandboxController(HelperConfig.NetworkConfig memory config) public returns (address sandboxController) {
-        uint256 sandboxControllerDeployerPrivateKey = vm.envUint("DAO_PRIVATE_KEY");
+        uint256 sandboxControllerDeployerPrivateKey = vm.envUint("SANDBOX_CONTROLLER_DEPLOYER_PRIVATE_KEY");
 
         ISandboxController.SandboxControllerConfiguration memory sandboxControllerConfig = ISandboxController
             .SandboxControllerConfiguration({
